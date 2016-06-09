069bf55c2eecc7403cb770176ba374986b54497c
Dave Nicholson <fatnic388@gmail.com>
2016-06-09 21:44:42 +0100

    initial commit

---
+++ js/libs/animator.js
@@ -0,0 +1,54 @@
+function Animator(image, frameWidth, frameHeight) {
+    this.image = image;
+    this.imgWidth = image.width;
+    this.imgHeight = image.height;
+    this.frameWidth = frameWidth;
+    this.frameHeight = frameHeight;
+    this.sequences = [];
+    this.currentSequence = null;
+    this.timer = 0;
+}
+
+Animator.prototype.setSequence = function (name, frames, delay) {
+    sequence = {};
+    sequence.name = name;
+    sequence.frames = frames;
+    sequence.delay = delay;
+    this.sequences.push(sequence);
+    if (this.currentSequence === null) { this.currentSequence = sequence; }
+};
+
+Animator.prototype.play = function (sequence) {
+    var seq = this.sequences.filter(function(s){ return s.name == sequence; });
+    if(seq) {
+        this.currentSequence = seq[0];
+        this.currentSequence.currentFrame = 0;
+    }
+};
+
+Animator.prototype.getFrame = function (delta) {
+
+    this.timer += delta;
+    var frameNo;
+
+    if(this.currentSequence.delay !== null) {
+        if(this.timer > this.currentSequence.delay) {
+            this.currentSequence.currentFrame++;
+            this.timer = 0;
+        }
+        frameNo = this.currentSequence.currentFrame % (this.imgWidth / this.frameWidth);
+    } else {
+        frameNo = 0;
+    }
+
+    var frame = Tools.i2xy(this.currentSequence.frames[frameNo] - 1, this.imgWidth/this.frameWidth);
+    var x = frame[0] * this.frameWidth;
+    var y = frame[1] * this.frameHeight;
+
+    var can = document.createElement('canvas');
+    var con = can.getContext('2d');
+    can.width = this.frameWidth;
+    can.height = this.frameHeight;
+    con.drawImage(this.image, x, y, can.width, can.height, 0, 0, this.frameWidth, this.frameHeight);
+    return can;
+};
---
+++ js/main.js
@@ -0,0 +1,57 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var now;
+var delta;
+var then = Date.now();
+var interval = 1000/30;
+
+var assets = {
+    base: '../assets/',
+    paths: ['gb_walk.png', 'coin.png']
+};
+
+var AM = new AssetManager(assets);
+AM.downloadAll(init);
+
+function Coin() {
+    this.anim = new Animator(AM.get('img.coin'), 44, 40);
+    this.anim.setSequence('still', [10], null);
+    this.anim.setSequence('spin', [1,2,3,4,5,6,7,8,9,10], 0.05);
+    this.position = new Vec2(canvas.width/2, canvas.height/2);
+
+    this.draw = function() {
+        ctx.drawImage(this.anim.getFrame(delta), this.position.x, this.position.y);
+    };
+}
+
+var coin = new Coin();
+// coin.anim.play('still');
+
+function init() {
+    loop();
+}
+
+function update() {
+    if (Key.isDown(Key.P)) { coin.anim.play('spin'); }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    coin.draw();
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+
+    now = Date.now();
+    delta = (now - then) / 1000;
+
+    update();
+    draw();
+
+    then = now;
+}
+
+// init is called when all assets are loaded
+// init();
